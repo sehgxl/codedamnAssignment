e8f352d0be1865aa227cf17a2acaa6248d804d2f
sehgxl <abhinavsehgal802@gmail.com>
2023-06-28 05:27:20 +0530

    fix: Change add button color from green to violet

--- src/components/certificates.tsx
+++ src/components/certificates.tsx
@@ -8,13 +8,7 @@ const Certificates: FunctionComponent<{ user: userType }> = ({ user }) => {
     <section className="flex flex-col pt-10">
       <div className="flex flex-col items-start justify-between gap-3 sm:flex-row sm:items-center">
         <h1 className="text-3xl font-bold">Certificates</h1>
-        <SecondaryBtn
-          state={true}
-          handleClick={() => {
-            console.log("Abhinav");
-          }}
-          btnText="Add New Certificate"
-        />
+        <SecondaryBtn state={true} btnText="Add New Certificate" />
       </div>
       <div className="grid grid-cols-1 grid-rows-4 gap-5 pt-6 sm:grid-cols-2 sm:grid-rows-2">
         {user?.certificates.map((certificate, index) => {
